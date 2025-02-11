2d7dd2ae0c5d6d5f578a4898a72046b155c76618
steevencalispa1 <marlocalispa@gmail.com>
2025-02-11 06:50:02 -0500

    sellos con bootstrap

--- src/componentes/Sellos.jsx
+++ src/componentes/Sellos.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 // Datos de los sellos
 const sellos = [
@@ -11,30 +12,6 @@ const sellos = [
   { src: "/img/sello7.png", title: "Sello Boca Jr" },
   { src: "/img/sello8.png", title: "Sello Personalizado" },
   { src: "/img/sello9.png", title: "Sello Ecuador" },
-  { src: "/img/sello10.png", title: "Sello Brasil" },
-  { src: "/img/sello11.png", title: "Sello España" },
-  { src: "/img/sello12.png", title: "Sello Pais" },
-  { src: "/img/sello13.png", title: "Sello Emelec" },
-  { src: "/img/sello14.png", title: "Sello Personalizado" },
-  { src: "/img/sello15.png", title: "Sello Bsc" },
-  { src: "/img/sello16.png", title: "Sello Liga de Quito" },
-  { src: "/img/sello17.png", title: "Sello Equipo" },
-  { src: "/img/sello18.png", title: "Sello Paris Saint-Germain" },
-  { src: "/img/sello19.png", title: "Sello Independiente" },
-  { src: "/img/sello20.png", title: "Sello Paris" },
-  { src: "/img/sello21.png", title: "Sello Pais" },
-  { src: "/img/sello22.png", title: "Sello Real Madrid" },
-  { src: "/img/sello23.png", title: "Sello Personalizado" },
-  { src: "/img/sello24.png", title: "Sellos Industria Textil Logos" },
-  { src: "/img/sello25.png", title: "Personalizacion de sellos" },
-  { src: "/img/sello26.png", title: "Logos y Marcas de Equipos" },
-  { src: "/img/sello27.png", title: "Sello Personalizado" },
-  { src: "/img/sello28.png", title: "Sello Rick and Morty" },
-  { src: "/img/sello29.png", title: "Sello Tom and Jerry" },
-  { src: "/img/sello30.png", title: "Sello Barcelona de España" },
-  { src: "/img/sello31.png", title: "Sello Lacosta" },
-  { src: "/img/sello32.png", title: "Sello Stitch" },
-  { src: "/img/sello33.png", title: "Sello Personalizado" },
 ];
 
 const SellosTextiles = () => {
@@ -43,20 +20,28 @@ const SellosTextiles = () => {
   };
 
   return (
-    <div >
-      <h1 className="title_txt">SELLOS TEXTILES PERSONALIZADOS</h1>
-      <section className="container">
+    <div className="container py-4 bg-dark text-light">
+      <h1 className="text-center text-warning py-3">
+        SELLOS TEXTILES PERSONALIZADOS
+      </h1>
+      <div className="row justify-content-center">
         {sellos.map((sello, index) => (
-          <div className="container_hijo" key={index}>
-            <img
-              src={sello.src}
-              alt={sello.title}
-              onClick={() => openModal(index)}
-            />
-            <h1 className="tittle_img">{sello.title}</h1>
+          <div className="col-md-4 mb-4" key={index}>
+            <div className="card bg-secondary text-light text-center shadow">
+              <img
+                src={sello.src}
+                alt={sello.title}
+                className="card-img-top p-3 rounded"
+                onClick={() => openModal(index)}
+                style={{ cursor: "pointer" }}
+              />
+              <div className="card-body">
+                <h5 className="card-title text-warning">{sello.title}</h5>
+              </div>
+            </div>
           </div>
         ))}
-      </section>
+      </div>
     </div>
   );
 };
